6c5e905cb98895f9ac21cbad90aa4d4b92317a40
Dhanush653 <dk652003@gmail.com>
2024-06-18 15:12:27 +0530

    updated

--- src/main/java/com/example/retrospect/websockets/SocketModule.java
+++ src/main/java/com/example/retrospect/websockets/SocketModule.java
@@ -40,46 +40,6 @@ public class SocketModule {
         };
     }
 
-//    private ConnectListener onConnected() {
-//        return (client) -> {
-//            var params = client.getHandshakeData().getUrlParams();
-//            String room = String.join("", params.getOrDefault("room", List.of("default")));
-//            String username = String.join("", params.getOrDefault("username", List.of("Anonymous")));
-//            client.joinRoom(room);
-//            log.info("Client [{}] joined room [{}] with username [{}]", client.getSessionId(), room, username);
-//
-//            // Log all clients in the room
-//            var clients = server.getRoomOperations(room).getClients();
-//            log.info("Room [{}] has [{}] clients", room, clients.size());
-//
-//            // Log handshake data
-//            var handshakeData = client.getHandshakeData();
-//            log.info("Handshake data: {}", handshakeData);
-//
-//            // Log if any session attributes are set
-//            log.info("Session ID [{}] - User [{}]", client.getSessionId(), client.getSessionId().toString());
-//
-//            socketService.saveInfoMessage(client, String.format(Constants.WELCOME_MESSAGE, username), room, username);
-//        };
-//    }
-//
-//    private DisconnectListener onDisconnected() {
-//        return client -> {
-//            var params = client.getHandshakeData().getUrlParams();
-//            String room = String.join("", params.getOrDefault("room", List.of("default")));
-//            String username = String.join("", params.getOrDefault("username", List.of("Anonymous")));
-//
-//            // Log disconnection reason if available
-//            log.info("Client [{}] disconnected from room [{}] with username [{}]", client.getSessionId(), room, username);
-//
-//            socketService.saveInfoMessage(client, String.format(Constants.EXIST_MESSAGE, username), room, username);
-//            client.leaveRoom(room); // Ensure client leaves the room
-//
-//            // Log remaining clients in the room
-//            var clients = server.getRoomOperations(room).getClients();
-//            log.info("Room [{}] has [{}] clients remaining", room, clients.size());
-//        };
-//    }
 private ConcurrentHashMap<UUID, Long> clientLastConnectionTime = new ConcurrentHashMap<>();
 
     private ConnectListener onConnected() {
@@ -127,7 +87,4 @@ private ConcurrentHashMap<UUID, Long> clientLastConnectionTime = new ConcurrentH
             log.info("Client [{}] disconnected due to [{}]", client.getSessionId(), disconnectReason);
         };
     }
-
-
-
 }
